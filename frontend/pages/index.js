fa9a49b76b017ff09e6fd4d6b74a2698c0dc69e4
chris <andersan@umich.edu>
2022-10-27 10:35:47 -0500

    Add favicon and title to index

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,6 @@
 import dynamic from 'next/dynamic'
 import {useState} from "react";
+import Head from 'next/head';
 
 const CesiumViewer = dynamic(
   () => import('../components/CesiumViewer/CesiumViewer'),
@@ -16,6 +17,12 @@ export default function Home() {
 
   return (
     <>
+      <Head>
+        <title>Radio map</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
+        {/* <a href="https://www.vecteezy.com/free-vector/globe-icon">Globe Icon Vectors by Vecteezy</a> */}
+      </Head>
       <RadioMenu selectedPlace={selectedPlace} selectedChannel={selectedChannel}></RadioMenu>
       <CesiumViewer setSelectedPlace={setSelectedPlace} setSelectedChannel={setSelectedChannel}></CesiumViewer>
     </>
